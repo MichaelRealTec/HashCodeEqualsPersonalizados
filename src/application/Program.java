0d59b360535f4292c6640451fbba0c38e7a4f9c8
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-26 18:20:34 -0300

    HashCode e Equals (exemplos práticos)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,26 @@
 package application;
 
+import entities.Client;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
+		String s1 = "Test";
+		String s2 = "Test";
+		
+		String s3 = new String("Test");
+		String s4 = new String("Test");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2)); 
+		System.out.println(c1 == c2); // false = compara as referências de memória por isto da 'false'
+		System.out.println(s1 == s2); // true = compara a expressão literal de forma especial para literais 
+		System.out.println(s3 == s4); // false = compara os objetos como distintos
 
 	}
 
